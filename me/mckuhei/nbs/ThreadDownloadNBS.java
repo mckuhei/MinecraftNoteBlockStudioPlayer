194ce525b9ffdabdb3e09039589fb26bb7b73862
kebufu <mcku_hei@qq.com>
2021-07-21 20:06:34 +0800

    update.

--- me/mckuhei/nbs/ThreadDownloadNBS.java
+++ me/mckuhei/nbs/ThreadDownloadNBS.java
@@ -16,16 +16,15 @@ public class ThreadDownloadNBS extends Thread {
 	}
 	public void run() {
 		try {
-			player.sendMessage(Main.plugin.config.getString("downloading"));
+			player.sendMessage(Main.plugin.pluginPrefix+Main.plugin.config.getString("downloading"));
 			URLConnection conn=this.url.openConnection();
 			conn.setDoInput(true);
 			conn.connect();
 			NBS nbs=new NBS(new MyInputStream(conn.getInputStream()));
-			player.sendMessage(Main.plugin.config.getString("downloaded"));
+			player.sendMessage(Main.plugin.pluginPrefix+Main.plugin.config.getString("downloaded"));
 			if(!Main.plugin.checkSong(nbs, player)) {
 				return;
 			}
-			System.out.println(nbs.notes.length);
 			synchronized(Main.playerThread.players){
 				Main.playerThread.players.add(new SongPlayer(player,nbs));
 			}
